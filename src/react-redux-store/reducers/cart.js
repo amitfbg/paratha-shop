738232371e733ae889b5fdcbf5f6a0a5f422bc5d
Amit Kumar <Amit.Kumar338@T-Mobile.com>
2022-03-06 12:47:31 +0530

    Cart reducer updated

--- src/react-redux-store/reducers/cart.js
+++ src/react-redux-store/reducers/cart.js
@@ -1,23 +1,9 @@
 const initialState = {
   items: [],
   itemCount: 0,
-  totalItemsPrice: 0,
   deliveryCharge: 0,
-  total: 0,
 };
 
-const getTopSum = (topArray, toppingsPrices) => {
-  return topArray.reduce((acc, curr) => {
-    return acc + toppingsPrices[curr];
-  }, 0);
-};
-
-const getTotalSum = (totalItem) => {
-  return totalItem.reduce((acc, currObj) => {
-    const currSum = currObj?.cost;
-    return acc + currSum;
-  }, 0);
-};
 const getTotalCount = (totalItem) => {
   return totalItem.reduce((acc, currObj) => {
     const currSum = currObj?.count;
@@ -26,14 +12,10 @@ const getTotalCount = (totalItem) => {
 };
 
 const getUpdatedValues = (state) => {
-  const updatedPrice = getTotalSum(state.items);
   const updatedCount = getTotalCount(state.items);
-  const updatedTotal = Number(state.deliveryCharge) + Number(updatedPrice);
   return {
     ...state,
-    totalItemsPrice: updatedPrice,
     itemCount: updatedCount,
-    total: updatedTotal,
   };
 };
 
@@ -44,26 +26,19 @@ const cart = (state = initialState, action) => {
       const found = state.items.findIndex(
         (currObj) => currObj?.id === action?.payload?.id
       );
-      // calculating ADD ON cost
-      const topSum = getTopSum(
-        action?.payload?.top,
-        action?.payload?.toppingsPrices
-      );
-      // calculating each Paratha price with or without add on
-      let priceParatha = Number(topSum) + Number(action?.payload?.price);
+
       // creating new data to be inserted
       let newData = {};
-      newData.priceParatha = priceParatha;
+      newData.priceParatha = Number(action?.payload?.price);
       newData.top = action?.payload?.top;
+
       // If found calculating the cost
       if (found !== -1) {
         newData = { ...state.items[found], ...newData };
-        newData.cost = priceParatha * state.items[found].count;
-        state.items.splice(found, 1, newData);
+        state.items[found] = { ...state.items[found], ...newData };
       } else {
         newData = { ...newData, ...action.payload };
-        newData.cost = priceParatha;
-        state.items.push(newData);
+        state.items = state.items.concat(newData);
       }
       //updating the values
       return getUpdatedValues(state);
@@ -73,14 +48,12 @@ const cart = (state = initialState, action) => {
       return {
         items: [],
         itemCount: 0,
-        totalItemsPrice: 0,
         deliveryCharge: 0,
-        total: 0,
       };
     }
 
     case "REMOVE_CART_ITEM": {
-      state.items.splice(action.payload, 1);
+      state.items = state.items.filter((_, idx) => idx !== action.payload);
       if (state.items.length === 0) {
         state.deliveryCharge = 0;
       }
@@ -96,9 +69,7 @@ const cart = (state = initialState, action) => {
       if (found !== -1) {
         newData = { ...state.items[found] };
         newData.count = state.items[found].count + 1;
-        newData.cost =
-          state.items[found].priceParatha * (state.items[found].count + 1);
-        state.items.splice(found, 1, newData);
+        state.items[found] = { ...state.items[found], ...newData };
       }
       //updating the values
       return getUpdatedValues(state);
@@ -111,13 +82,11 @@ const cart = (state = initialState, action) => {
       let newData = {};
       if (found !== -1) {
         if (state.items[found].count - 1 === 0) {
-          state.items.splice(found, 1);
+          state.items = state.items.filter((_, idx) => idx !== found);
         } else {
           newData = { ...state.items[found] };
           newData.count = state.items[found].count - 1;
-          newData.cost =
-            state.items[found].priceParatha * (state.items[found].count - 1);
-          state.items.splice(found, 1, newData);
+          state.items[found] = { ...state.items[found], ...newData };
         }
       }
       //updating the values
@@ -125,16 +94,9 @@ const cart = (state = initialState, action) => {
     }
 
     case "ADD_Delivery": {
-      const newDeliveryCharge = action?.payload;
-      //updating the values
-      const updatedPrice = getTotalSum(state.items);
-      const updatedCount = getTotalCount(state.items);
-      const updatedTotal = Number(newDeliveryCharge) + Number(updatedPrice);
+      const newDeliveryCharge = Number(action?.payload);
       return {
         ...state,
-        totalItemsPrice: updatedPrice,
-        itemCount: updatedCount,
-        total: updatedTotal,
         deliveryCharge: newDeliveryCharge,
       };
     }
